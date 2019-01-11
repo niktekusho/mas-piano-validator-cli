2587b9e0a151089152d32205d57e31f8522e6893
Nicola Dal Maso <nicola.dalmaso@outlook.com>
2019-01-11 16:30:33 +0100

    Tests after refactor. Still missing a test case.

---
+++ __mocks__/signale.js
@@ -0,0 +1,19 @@
+const EventEmitter = require('events');
+
+const signale = jest.genMockFromModule('signale');
+
+signale.emitter = new EventEmitter();
+
+// Stub every function
+for (const property in signale) {
+	if (Object.prototype.hasOwnProperty.call(signale, property)) {
+		const value = signale[property];
+		if (typeof value === 'function') {
+			signale[property] = function (args) {
+				this.emitter.emit(`signale_${property}`, args);
+			};
+		}
+	}
+}
+
+module.exports = signale;
--- test.js
+++ test.js
@@ -1,7 +1,72 @@
+const {EOL} = require('os');
+
 const execa = require('execa');
+const signale = require('signale');
+
+const main = require('.');
 
 describe('Spawning the CLI in an other process', () => {
 	it('should do stuff', async () => {
 		await execa('./cli.js', ['./examples/example.json']).stdout.pipe(process.stdout);
+	});
+});
+
+describe('Testing CLI\'s "business logic"', () => {
+	// "emitter" is defined in the manual mock
+	const {emitter} = signale;
+	const timeout = 2000;
+
+	it('should return appropriate message when no input is specified', async () => {
+		const result = await new Promise((resolve, reject) => {
+			emitter.on('signale_error', args => {
+				resolve(args);
+			});
+
+			// After 1 sec. reject the promise
+			setTimeout(() => reject(new Error('Timeout reached!')), timeout);
+
+			main([]);
+		});
+		expect(result).toStrictEqual('Specify at least one path!');
+	});
+
+	it('should return appropriate message when a valid input file is specified', async () => {
+		const result = await new Promise((resolve, reject) => {
+			emitter.on('signale_info', args => {
+				resolve(args);
+			});
+			// After set timeout reject the promise
+			setTimeout(() => reject(new Error('Timeout reached!')), timeout);
+
+			main(['examples/example.json']);
+		});
+		expect(result).toStrictEqual('This file is a valid Monika After Story piano song!');
+	});
+
+	it('should return appropriate message when a invalid input file is specified', async () => {
+		const result = await new Promise((resolve, reject) => {
+			emitter.on('signale_info', args => {
+				resolve(args);
+			});
+			// After set timeout reject the promise
+			setTimeout(() => reject(new Error('Timeout reached!')), timeout);
+
+			main(['examples/invalidsong.json']);
+		});
+		expect(result).toStrictEqual('This file is NOT a valid Monika After Story piano song.');
+	});
+
+	it('should return appropriate message when a directory is specified', async () => {
+		const result = await new Promise((resolve, reject) => {
+			emitter.on('signale_info', args => {
+				resolve(args);
+			});
+			// After set timeout reject the promise
+			setTimeout(() => reject(new Error('Timeout reached!')), timeout);
+
+			main(['examples']);
+		});
+		// The passed string must span on multiple lines
+		expect(result.split(EOL).length > 1).toBeTruthy();
 	});
 });
