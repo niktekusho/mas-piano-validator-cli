94349351b99cfd5b52dda8c75a4689786141a6d3
niktekusho <nicola.dalmaso@outlook.com>
2019-01-14 17:25:15 +0100

    Initial handling of multiple arguments

--- index.js
+++ index.js
@@ -17,16 +17,46 @@ async function main(input) {
 		logger.error('Specify at least one path!');
 		return;
 	}
-	// TODO: Handle other args
-	const normalizedInput = path.normalize(input[0]);
-	let result = null;
-	try {
-		result = await processPath(normalizedInput);
-	} catch (error) {
-		logger.error(error.message);
+
+	// Preprocess all arguments
+	const validationItems = [];
+	// TODO: evaluate other ways to do this
+	/* eslint-disable no-await-in-loop */
+	for (const i of input) {
+		const normalizedInput = path.normalize(i);
+		// TODO: remove before merge
+		logger.debug(normalizedInput);
+
+		if (normalizedInput.endsWith('.json')) {
+			const content = await readFile(normalizedInput, {encoding: 'utf8'});
+			const validationItem = createValidationItem(normalizedInput, content);
+			validationItems.push(validationItem);
+			return;
+		}
+		const pathStat = await stat(normalizedInput);
+		if (pathStat.isDirectory()) {
+			const children = await readdir(normalizedInput);
+
+			// Run the validator only on json files
+			const jsonChildrens = children.filter(child => child.endsWith('.json'));
+			const jsonChildrensPath = jsonChildrens.map(jsonChild => path.join(normalizedInput, jsonChild));
+			const childrenResultPromises = jsonChildrensPath.map(async childPath => {
+				return createValidationItem(childPath, await readFile(childPath, {encoding: 'utf8'}));
+			});
+			validationItems.push(...await Promise.all(childrenResultPromises));
+		} else {
+			logger.warn(`Ignoring ${normalizedInput} since it is not a JSON file.`);
+		}
+	}
+
+	if (validationItems.length === 0) {
+		// TODO: better msg
+		logger.warn('No JSON files found.');
 		return;
 	}
-	// If, for whatever reason, result is still null, "let it die!"
+
+	const result = validate.all(validationItems);
+
 	// Both ValidationResult and ValidationResultsContainer have the ok and summary properties (they implement the same "interface")
 	const globalMsg = result.summary;
 	if (result.ok) {
@@ -38,6 +68,15 @@ async function main(input) {
 	if (result.results) {
 		printChildrenResults(result.results);
 	}
+}
+
+function createValidationItem(filePath, content) {
+	return {
+		meta: {
+			src: filePath
+		},
+		content
+	};
 }
 
 function printChildrenResults(results) {
@@ -53,57 +92,6 @@ function printChildrenResults(results) {
 			sublogger.warn(logObject);
 		}
 	});
-}
-
-/**
- * Process the file or the files inside the specified path.
- *
- * General algorithm:
- *
- * 1. If the path ends with .json process the file immediately, otherwise move on.
- * 2. If the path is a directory (fs.stat), then process all the JSON files in the directory.
- * 3. If none of the above is true, throw an Error saying the path could point to JSON files.
- *
- * @param {string} p Path to check
- * @returns {Promise<ValidationResult|ValidationResultsContainer>} Validation result.
- */
-async function processPath(p) {
-	if (p.endsWith('.json')) {
-		return processFile(p);
-	}
-	const pathStat = await stat(p);
-	if (pathStat.isDirectory()) {
-		return processDir(p);
-	}
-	throw new Error('The path received did not point to JSON files.');
-}
-
-/**
- * Retrieve all the children of the directory and process all JSON files found.
- *
- * @param {string} dir Directory to process
- */
-async function processDir(dir) {
-	const children = await readdir(dir);
-
-	// Run the validator only on json files
-	const jsonChildrens = children.filter(child => child.endsWith('.json'));
-	const jsonChildrensPath = jsonChildrens.map(jsonChild => path.join(dir, jsonChild));
-	const childrenResultPromises = jsonChildrensPath.map(async childPath => {
-		return {
-			meta: {
-				src: childPath
-			},
-			content: await readFile(childPath, {encoding: 'utf8'})
-		};
-	});
-	const childrenResults = await Promise.all(childrenResultPromises);
-	return validate.all(childrenResults);
-}
-
-async function processFile(file) {
-	const content = await readFile(file, {encoding: 'utf8'});
-	return validate(content, {src: file});
 }
 
 module.exports = main;
