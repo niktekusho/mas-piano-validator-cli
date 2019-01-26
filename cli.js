8959286a579c5e306a124d94d11cbf27cb00ade3
Nicola <nicola.dalmaso@outlook.com>
2019-01-26 21:37:10 +0100

    Added check for -h flag. Fixes #8.

--- cli.js
+++ cli.js
@@ -9,4 +9,11 @@ const cli = meow(`
 	  $ mas-piano-validator <input>
 `);
 
+// Meow by default does not support the alias for --help, so this is the suggested solution
+// (https://github.com/sindresorhus/meow/issues/46#issuecomment-234776549)
+if (cli.flags.h) {
+	// Automatically calls process.exit
+	cli.showHelp();
+}
+
 main(cli.input);
